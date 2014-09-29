77e48f754510f6190e30e2e668061e1a9046c42e
unknown <linggarp19@gmail.com>
2014-09-29 10:42:37 +0700

    commit 1

---
+++ nama.java
@@ -0,0 +1,9 @@
+public class nama {
+      public static void main(String[] args) {
+	  
+	  System.out.println("Nama Kelompok Tabungan SA");
+	  System.out.println("- Lina Purwasari (1127050087)");
+	  System.out.println("- Linggar Dwi P  (1127050088");
+	  
+	  }
+}
